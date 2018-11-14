6adf95ab983d77aafd65492cbf1c790c5383b964
GriffinMartin <gmart248@gmail.com>
2018-11-14 00:53:28 -0700

    Veutify material-ui added. Setup basic structure of app.

---
+++ src/components/Shows.vue
@@ -0,0 +1,16 @@
+<template>
+  <Show />
+</template>
+
+<script>
+import Show from "../components/Show";
+
+export default {
+  components: {
+    Show
+  },
+  data: () => ({})
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <Shows />
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Shows from "../components/Shows";
 
 export default {
-  name: "home",
   components: {
-    HelloWorld
+    Shows
   }
 };
 </script>
